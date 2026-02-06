cd406fee6ad31e02383231b7c2bc15553d7177ad
Torin <tcostal2@pdx.edu>
2026-02-06 14:30:41 -0800

    as good as its gonna get

--- tarasaur.c
+++ tarasaur.c
@@ -238,167 +238,262 @@ int main(int argc, char *argv[]){
 			fprintf(stderr, "version mismatch 2\n");
 			exit(BAD_MAGIC);
 		}
-		
-		if(action == ACTION_VALIDATE){
-			struct node* head = NULL, *tail = NULL;
-			off_t end, start;
 
+                 if(action == ACTION_EXTRACT){
+                         struct node* head = NULL, *tail = NULL;
+			 unsigned char buff[COPY_BUF_SIZE];
+                         off_t end, start;
 			if(read(iarch, &num_members, sizeof(num_members)) != sizeof(num_members)){
-				if(filename) close(iarch);
-				exit(VALIDATE_ERROR);
-			}
-			end = lseek(iarch, 0, SEEK_END);
-			if(end == (off_t)-1){
-				if(filename) close(iarch);
-				exit(VALIDATE_ERROR);
-			}
+                         	exit(READ_FAIL);
+                 	}
 
-			start = end - (off_t)num_members* (off_t)sizeof(tarasaur_directory_t);
-			if(start <0){
-				if(filename) close(iarch);
-				exit(VALIDATE_ERROR);
-			}
-			if(lseek(iarch, start, SEEK_SET) == (off_t)-1){
-				if(filename) close(iarch);
-				exit(VALIDATE_ERROR);
-			}
-			for(int i = 0; i < num_members; i++){
-				struct node * nd = calloc(1, sizeof(*nd));
-				if(!nd){
-					free_list(head);
-					if(filename) close(iarch);
-					exit(VALIDATE_ERROR);
-				}
-				if(read(iarch, &nd->hdr, sizeof(nd->hdr)) != sizeof(nd->hdr)){
-					free(nd);
-					free_list(head);
-					if(filename) close(iarch);
-					exit(VALIDATE_ERROR);
-				}
-				if(!head) head = tail = nd;
-				else { tail->next = nd; tail = nd;}
-			}
+                         end = lseek(iarch, 0, SEEK_END);
+                         if(end == (off_t)-1) exit(EXTRACT_FAIL);
 
-			for(struct node* cur = head; cur; cur = cur->next){
-				uint32_t crc_d, stored_hdr, crc32_hdr;
-				size_t left;
-				size_t stored_size = 0;
-				tarasaur_directory_t dir = cur->hdr;
-				if(read(iarch, &stored_size, sizeof(stored_size)) != sizeof(stored_size)){
-					free_list(head);
-					if(filename) close(iarch);
-					exit(VALIDATE_ERROR);
-				}
-				if(stored_size != dir.tarasaur_size){
-					free_list(head);
-					if(filename) close(iarch);
-					exit(VALIDATE_ERROR);
-				}
-				crc_d = crc32(0L, Z_NULL, 0);
-				left = dir.tarasaur_size;
-				while (left > 0){
-					ssize_t n = read(iarch, buf, MIN(left, COPY_BUF_SIZE));
-					if (n <= 0){
+                         start = end- (off_t)num_members *(off_t)sizeof(tarasaur_directory_t);
+                         if (start <0) exit(EXTRACT_FAIL);
+
+                         if(lseek(iarch, start, SEEK_SET) == (off_t)-1) {exit(EXTRACT_FAIL);}
+
+                         for(int i = 0; i < num_members; i++){
+                                 struct node * nd = calloc(1, sizeof(*nd));
+                                 if(!nd){
+                                         free_list(head);
+                                         if(filename) close(iarch);
+                                         exit(EXTRACT_FAIL);
+                                 }
+                         
+                         if(read(iarch, &nd->hdr, sizeof(nd->hdr)) != sizeof(nd->hdr)){
+                                 free(nd);
+                                 free_list(head);
+                                 if(filename) close(iarch);
+                                 exit(EXTRACT_FAIL);
+                         }
+                         if(!head) head = tail = nd;
+                         else { tail->next = nd; tail = nd;}
+			 }
+
+			 for(struct node *cur = head; cur; cur = cur->next){
+				 int ofd;
+				 ssize_t left, off, n, w, chunk;
+				 struct timespec ts[2];
+				 char name[TARASAUR_MAX_NAME_LEN +1];
+				 tarasaur_directory_t *h = &cur->hdr;
+				 memcpy(name, h->tarasaur_name, TARASAUR_MAX_NAME_LEN);
+				 name[TARASAUR_MAX_NAME_LEN]= '\0';
+
+				 ofd = open(name, O_WRONLY | O_CREAT | O_TRUNC, h->tarasaur_mode);
+				 if(ofd < 0) {
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(EXTRACT_FAIL);
+				 }
+
+				 if(lseek(iarch, h->tarasaur_data_offset, SEEK_SET) == (off_t)-1){
+					 close(ofd);
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(EXTRACT_FAIL);
+				 }
+
+				left = h->tarasaur_size;
+				while(left > 0){
+					chunk = MIN(left, COPY_BUF_SIZE);
+					n = read(iarch, buff, chunk);
+					if(n <= 0){
+						close(ofd);
 						free_list(head);
 						if(filename) close(iarch);
-						exit(VALIDATE_ERROR);
+						exit(EXTRACT_FAIL);
 					}
-					crc_d = crc32(crc_d, (Bytef*)buf, (uInt)n);
+
+					off = 0; 
+					while(off < n){
+						w = write(ofd, buff + off, (size_t)(n-off));
+						if(w <=0){
+						close(ofd);
+						free_list(head);
+						if(filename) close(iarch);
+						exit(EXTRACT_FAIL);
+						}
+						off += w;
+					}
 					left -= (size_t)n;
 				}
-				if(crc_d != cur->hdr.crc32_data){
-					free_list(head);
-					if(filename) close(iarch);
-					exit(VALIDATE_ERROR);
-				}
-				stored_hdr = cur->hdr.crc32_header;
-				dir.crc32_data =0;
-				dir.crc32_header =0;
-				crc32_hdr = crc32(0L, (Bytef *)&dir, (uInt)sizeof(dir));
-				if(crc32_hdr != stored_hdr){
-					free_list(head);
-					if(filename) close(iarch);
-					exit(VALIDATE_ERROR);
-				}
-			}
-		free_list(head);
-		if(filename) close(iarch);
-		}
-		
-		//small TOC and large TOC reading  
-		if(action == ACTION_TOC_SHORT || action == ACTION_TOC_LONG){
-			read(iarch, &num_members, sizeof(num_members));
 
-			for(int i = 0; i < num_members; ++i){
-				size_t data_size = 0;
-				read(iarch, &data_size, sizeof(data_size));
-				if(is_verbose){
-					fprintf(stderr, "\tskip over data %d of %zd bytes\n", i, data_size);
-				}
-				lseek(iarch, data_size, SEEK_CUR);
-			}
-			if(filename){
-				if(is_verbose) {printf("Reading archive file: " " %s\n", filename);}
-				printf("Table of contents of tarannosaurus file: "
-						"\"%s\" with %d members\n",
-						filename,
-						num_members);
-			}
-			else{
-				printf("Table of contents tarannosaurus from" "stdin with %d members\n", num_members);
-			}
-			for (int i =0; i <num_members; ++i){
-				char buffer[TARASAUR_MAX_NAME_LEN *2] ={'\0'};
-				tarasaur_directory_t header;
+				(void)fchmod(ofd, h->tarasaur_mode);
+				close(ofd);
+				ts[0] = h->tarasaur_atim;
+				ts[1] = h->tarasaur_mtim;
+				(void)utimensat(AT_FDCWD, h->tarasaur_name, ts, 0);
+			 }
+			 free_list(head);
+			 if(filename) close(iarch);
 
-				memset(buffer, 0, sizeof(buffer));
-				read(iarch, &header, sizeof(tarasaur_directory_t));
-				strncpy(buffer, header.tarasaur_name, TARASAUR_MAX_NAME_LEN);
-				printf("\tfile name: %s\n", buffer);
-				//logic for large TOC 
-				if(action == ACTION_TOC_LONG){
-					//file permissions
-					printf("\t\tmode:\t" );
-					printf("%c", S_ISDIR(header.tarasaur_mode) ? 'd' : '-');
-					printf("%c", (header.tarasaur_mode & S_IRUSR) ? 'r' : '-');
-					printf("%c", (header.tarasaur_mode & S_IWUSR) ? 'w' : '-');
-					printf("%c", (header.tarasaur_mode & S_IXUSR) ? 'x' : '-');
+		 }
 
-					printf("%c", (header.tarasaur_mode & S_IRGRP) ? 'r' : '-');
-					printf("%c", (header.tarasaur_mode & S_IWGRP) ? 'w' : '-');
-					printf("%c", (header.tarasaur_mode & S_IXGRP) ? 'x' : '-');
 
-					printf("%c", (header.tarasaur_mode & S_IROTH) ? 'r' : '-');
-					printf("%c", (header.tarasaur_mode & S_IWOTH) ? 'w' : '-');
-					printf("%c", (header.tarasaur_mode & S_IXOTH) ? 'x' : '-');
-					printf("\n");
-					//userID
-					userid = getpwuid(header.tarasaur_uid);
-					if(userid == NULL) {printf("\tuser: \t%d\n", header.tarasaur_uid);}
-					printf("\t\tuser:\t\t%s\n", userid->pw_name);
-					//group
-					groupid = getgrgid(header.tarasaur_gid);
-					if(groupid == NULL) {printf("\tgroup: \t%d\n", header.tarasaur_gid);}
-					printf("\t\tgroup:\t\t%s\n", groupid->gr_name);
-					//size
-					printf("\t\tsize:\t\t%zu\n", header.tarasaur_size);
-					//mtime and atime
-					tm_mtime = localtime(&header.tarasaur_mtim.tv_sec);
-					strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S %Z", tm_mtime);
-					printf("\t\tmtime:\t\t%s\n", time_buf);
-					tm_atime = localtime(&header.tarasaur_atim.tv_sec);
-					strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S %Z", tm_atime);
-					printf("\t\tatime:\t\t%s\n", time_buf);
-					//crc32 checksums
-					printf("\t\tcrc32 header:\t\t0x%08x\n", header.crc32_header);
-					printf("\t\tcrc32 data:\t\t0x%08x\n", header.crc32_data);
+		 if(action == ACTION_VALIDATE){
+			 struct node* head = NULL, *tail = NULL;
+			 off_t end, start;
+			 unsigned char v_buf[COPY_BUF_SIZE];
 
-				}
-			}
-			if (filename){
-				close(iarch);
-			}
-		}
+			 if(read(iarch, &num_members, sizeof(num_members)) != sizeof(num_members)){
+				 if(filename) close(iarch);
+				 exit(VALIDATE_ERROR);
+			 }
+			 end = lseek(iarch, 0, SEEK_END);
+			 if(end == (off_t)-1){
+				 if(filename) close(iarch);
+				 exit(VALIDATE_ERROR);
+			 }
+
+			 start = end - (off_t)num_members* (off_t)sizeof(tarasaur_directory_t);
+			 if(start <0){
+				 if(filename) close(iarch);
+				 exit(VALIDATE_ERROR);
+			 }
+			 if(lseek(iarch, start, SEEK_SET) == (off_t)-1){
+				 if(filename) close(iarch);
+				 exit(VALIDATE_ERROR);
+			 }
+			 for(int i = 0; i < num_members; i++){
+				 struct node * nd = calloc(1, sizeof(*nd));
+				 if(!nd){
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(VALIDATE_ERROR);
+				 }
+				 if(read(iarch, &nd->hdr, sizeof(nd->hdr)) != sizeof(nd->hdr)){
+					 free(nd);
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(VALIDATE_ERROR);
+				 }
+				 if(!head) head = tail = nd;
+				 else { tail->next = nd; tail = nd;}
+			 }
+
+			 for(struct node* cur = head; cur; cur = cur->next){
+				 uint32_t crc_d, stored_hdr, crc32_hdr;
+				 size_t left;
+				 size_t stored_size = 0;
+				 tarasaur_directory_t dir = cur->hdr;
+				 if(read(iarch, &stored_size, sizeof(stored_size)) != sizeof(stored_size)){
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(VALIDATE_ERROR);
+				 }
+				 if(stored_size != dir.tarasaur_size){
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(VALIDATE_ERROR);
+				 }
+				 crc_d = crc32(0L, Z_NULL, 0);
+				 left = dir.tarasaur_size;
+				 while (left > 0){
+					 ssize_t n = read(iarch, v_buf, MIN(left, COPY_BUF_SIZE));
+					 if (n <= 0){
+						 free_list(head);
+						 if(filename) close(iarch);
+						 exit(VALIDATE_ERROR);
+					 }
+					 crc_d = crc32(crc_d, (Bytef*)v_buf, (uInt)n);
+					 left -= (size_t)n;
+				 }
+				 if(crc_d != cur->hdr.crc32_data){
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(VALIDATE_ERROR);
+				 }
+				 stored_hdr = cur->hdr.crc32_header;
+				 dir.crc32_data =0;
+				 dir.crc32_header =0;
+				 crc32_hdr = crc32(0L, (Bytef *)&dir, (uInt)sizeof(dir));
+				 if(crc32_hdr != stored_hdr){
+					 free_list(head);
+					 if(filename) close(iarch);
+					 exit(VALIDATE_ERROR);
+				 }
+			 }
+			 free_list(head);
+			 if(filename) close(iarch);
+		 }
+
+		 //small TOC and large TOC reading  
+		 if(action == ACTION_TOC_SHORT || action == ACTION_TOC_LONG){
+			 read(iarch, &num_members, sizeof(num_members));
+
+			 for(int i = 0; i < num_members; ++i){
+				 size_t data_size = 0;
+				 read(iarch, &data_size, sizeof(data_size));
+				 if(is_verbose){
+					 fprintf(stderr, "\tskip over data %d of %zd bytes\n", i, data_size);
+				 }
+				 lseek(iarch, data_size, SEEK_CUR);
+			 }
+			 if(filename){
+				 if(is_verbose) {printf("Reading archive file: " " %s\n", filename);}
+				 printf("Table of contents of tarannosaurus file: "
+						 "\"%s\" with %d members\n",
+						 filename,
+						 num_members);
+			 }
+			 else{
+				 printf("Table of contents tarannosaurus from" "stdin with %d members\n", num_members);
+			 }
+			 for (int i =0; i <num_members; ++i){
+				 char buffer[TARASAUR_MAX_NAME_LEN *2] ={'\0'};
+				 tarasaur_directory_t header;
+
+				 memset(buffer, 0, sizeof(buffer));
+				 read(iarch, &header, sizeof(tarasaur_directory_t));
+				 strncpy(buffer, header.tarasaur_name, TARASAUR_MAX_NAME_LEN);
+				 printf("\tfile name: %s\n", buffer);
+				 //logic for large TOC 
+				 if(action == ACTION_TOC_LONG){
+					 //file permissions
+					 printf("\t\tmode:\t" );
+					 printf("%c", S_ISDIR(header.tarasaur_mode) ? 'd' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IRUSR) ? 'r' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IWUSR) ? 'w' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IXUSR) ? 'x' : '-');
+
+					 printf("%c", (header.tarasaur_mode & S_IRGRP) ? 'r' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IWGRP) ? 'w' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IXGRP) ? 'x' : '-');
+
+					 printf("%c", (header.tarasaur_mode & S_IROTH) ? 'r' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IWOTH) ? 'w' : '-');
+					 printf("%c", (header.tarasaur_mode & S_IXOTH) ? 'x' : '-');
+					 printf("\n");
+					 //userID
+					 userid = getpwuid(header.tarasaur_uid);
+					 if(userid == NULL) {printf("\tuser: \t%d\n", header.tarasaur_uid);}
+					 printf("\t\tuser:\t\t%s\n", userid->pw_name);
+					 //group
+					 groupid = getgrgid(header.tarasaur_gid);
+					 if(groupid == NULL) {printf("\tgroup: \t%d\n", header.tarasaur_gid);}
+					 printf("\t\tgroup:\t\t%s\n", groupid->gr_name);
+					 //size
+					 printf("\t\tsize:\t\t%zu\n", header.tarasaur_size);
+					 //mtime and atime
+					 tm_mtime = localtime(&header.tarasaur_mtim.tv_sec);
+					 strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S %Z", tm_mtime);
+					 printf("\t\tmtime:\t\t%s\n", time_buf);
+					 tm_atime = localtime(&header.tarasaur_atim.tv_sec);
+					 strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S %Z", tm_atime);
+					 printf("\t\tatime:\t\t%s\n", time_buf);
+					 //crc32 checksums
+					 printf("\t\tcrc32 header:\t\t0x%08x\n", header.crc32_header);
+					 printf("\t\tcrc32 data:\t\t0x%08x\n", header.crc32_data);
+
+				 }
+			 }
+			 if (filename){
+				 close(iarch);
+			 }
+		 }
 	}
 	return EXIT_SUCCESS;
 
